1d7b6367c745bc1ccb72340b73909252aee5d81e
sehyeon <tpgusdla5789@gmail.com>
2024-06-15 16:37:07 +0900

    day1

---
+++ _01_01.js
@@ -0,0 +1,11 @@
+console.log("hello world");
+console.log({name: '홍길동', age: 20, married: false});
+console.log('Hi!', 100, true, [1, 2, 3]);
+console.log(true);
+
+console.log('로그 - 기본적인 출력');
+console.info('로그 - 기능적으로는 log와 같음. 사용하기에 따라 용도 구분 가능');
+console.warn('경고 - 문제가 될 수 있는 부분');
+console.error('오류 - 에러 발생 상황');
+//브라우저 콘솔창에 치면 서로 다름
+//값을 출력하는 건 같지만 앞에 느낌표나 알림이나 그런게 달라짐
---
+++ _01_03.js
@@ -0,0 +1,12 @@
+//변수와 상수
+let x;
+console.log(x);
+x=1;
+console.log(x);
+x= "hello";
+console.log(x);
+
+const PI = 3.14;
+//값 변경 불가능, 선언할 때 초기화 해줘야함.
+
+
